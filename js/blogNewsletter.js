fcccfcca397e16115aed09982f7c84858844dc48
Code Club Academy <150022016+CCA-3@users.noreply.github.com>
2024-04-08 12:15:24 +0000

    Add files via upload

---
+++ js/blogNewsletter.js
@@ -0,0 +1,16 @@
+export const blogNewsletter = [
+  {
+    title: "FKF 1st Anniversary School Donation Supplies to AWMS JAN 19, 2024",
+    date: "March 27, 2024",
+    content:
+      "𝐅𝐫𝐚𝐧𝐜𝐢𝐬 𝐊𝐨𝐫𝐨𝐦𝐚 𝐅𝐨𝐮𝐧𝐝𝐚𝐭𝐢𝐨𝐧 𝐃𝐨𝐧𝐚𝐭𝐞𝐬 𝐒𝐜𝐡𝐨𝐨𝐥 𝐒𝐮𝐩𝐩𝐥𝐢𝐞𝐬 𝐭𝐨 𝐀𝐧𝐧𝐢𝐞 𝐖𝐚𝐥𝐬𝐡 𝐌𝐞𝐦𝐨𝐫𝐢𝐚𝐥 𝐒𝐞𝐜𝐨𝐧𝐝𝐚𝐫𝐲 𝐒𝐜𝐡𝐨𝐨𝐥 𝐢𝐧 𝐂𝐨𝐦𝐦𝐞𝐦𝐨𝐫𝐚𝐭𝐢𝐨𝐧 𝐨𝐟 𝟏𝐬𝐭 𝐀𝐧𝐧𝐢𝐯𝐞𝐫𝐬𝐚𝐫𝐲",
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    title: "FKF Speaker Series with Hon. Francis Ben Kaifala March 1,2024",
+    date: "March 27, 2024",
+    content:
+      "The Francis Koroma Foundation, in its effort to recognize individuals who are doing well in the public and private sectors of society, on 1st March 2024, the United States of America, conducted a special interview with the Anti Corruption Commissioner Francis-Ben Kaifala",
+    image: "https://via.placeholder.com/150",
+  },
+];
---
+++ js/main.js
@@ -0,0 +1,6 @@
+import { blogPosts } from "./blogPosts.js";
+let blogNewsletter = document.querySelector("#blog-newsletter");
+
+blogNewsletter.innerHTML = `<div class="container">
+
+</div>`;
